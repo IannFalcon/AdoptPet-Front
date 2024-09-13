ca230e81b18923954bfafae1743dee4125aca2f7
Jheicer Dev <JheicerDev@gmail.com>
2024-09-13 16:31:25 -0500

    Login and Menu

---
+++ app/src/main/java/com/example/frontendadoptpet/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.frontendadoptpet
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.frontendadoptpet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+}
